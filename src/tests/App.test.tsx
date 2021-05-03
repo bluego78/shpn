40d19d477f7314a4c9b7802492612bf6676f53be
Maurizio Podda <m.podda@consulcesi.com>
2021-05-03 23:15:43 +0200

    test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,10 +1,24 @@
 import { MockedContext } from '../contexts/MockAppContext';
 import App from '../components/App';
-import renderer from 'react-test-renderer';
+import renderer, { act } from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 
 describe('<App />', ()=>{
 
-    it('<App /> renders all the elements', ()=>{
+    let component:any;
+
+    beforeAll(()=>{
+        act(()=>{
+            component = renderer.create(<MockedContext><App /></MockedContext>);
+        });
+    });
+
+    afterAll(()=>{
+        component = null;
+    });
+
+    it('Renders properly', ()=>{
         
         let component = renderer.create(
             <MockedContext>
@@ -13,18 +27,24 @@ describe('<App />', ()=>{
           );
 
         expect(component.toJSON()).toMatchSnapshot();
-        expect(component.root.findAllByProps({id: "app-container"}).length).toBe(1); 
-        expect(component.root.findAllByProps({id: "SearchField"}).length).toBe(1);  
-        expect(component.root.findAllByProps({className: "users-list"}).length).toBe(1); 
-        expect(component.root.findAllByProps({className: "badge-container"}).length).toBe(2); 
-        expect(component.root.findAllByProps({className: "userbadge-container"}).length).toBe(2);
-        expect(component.root.findAllByProps({className: "nat-flag"}).length).toBe(2);
-        expect(component.root.findAllByProps({className: "picture"}).length).toBe(2);
-        expect(component.root.findAllByProps({className: "fullname"}).length).toBe(2);
-        expect(component.root.findAllByProps({className: "username"}).length).toBe(2);
-        expect(component.root.findAllByProps({className: "email"}).length).toBe(2);
         
-
     });
 
+    it('Renders the app-container', ()=>{
+        expect(component.root.findAllByProps({id: "app-container"}).length).toBe(1); 
+    });
+
+
+    it('AppContext is properly populated',()=>{
+        let app = shallow(<MockedContext><App /></MockedContext>);
+        let appContext = toJson(app).props.value;
+        expect(appContext.usersList.length).toBe(2);
+        expect(appContext.filteredUsersList.length).toBe(2);
+        expect(appContext._currentValue.isLoading).toBeFalsy();
+        expect(appContext._currentValue.modalIsOpened).toBeFalsy();
+        expect(appContext._currentValue.filterIsActive).toBeFalsy();
+        expect(appContext._currentValue.currentPage).toEqual(0);
+        expect(appContext._currentValue.totalResults).toEqual(0);
+    });
+    
 });
